1051ecc2ffe3fc8ef7be5d0f3caf59e3b14a8af4
Gerald Petron <jjtron@Geralds-Mac-mini.local>
2024-01-26 16:04:02 -0500

    Update per tutorial up to and including all of Ch 11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -140,11 +140,11 @@ export async function fetchInvoicesPages(query: string) {
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE
-      customers.name ILIKE ${`%${query}%`} OR
-      customers.email ILIKE ${`%${query}%`} OR
-      invoices.amount::text ILIKE ${`%${query}%`} OR
-      invoices.date::text ILIKE ${`%${query}%`} OR
-      invoices.status ILIKE ${`%${query}%`}
+      customers.name ILIKE ${`'%${query}%'`} OR
+      customers.email ILIKE ${`'%${query}%'`} OR
+      invoices.amount::text ILIKE ${`'%${query}%'`} OR
+      invoices.date::text ILIKE ${`'%${query}%'`} OR
+      invoices.status ILIKE ${`'%${query}%'`}
   `);
 
     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
@@ -213,8 +213,8 @@ export async function fetchFilteredCustomers(query: string) {
 		FROM customers
 		LEFT JOIN invoices ON customers.id = invoices.customer_id
 		WHERE
-		  customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`}
+		  customers.name ILIKE ${`'%${query}%'`} OR
+        customers.email ILIKE ${`'%${query}%'`}
 		GROUP BY customers.id, customers.name, customers.email, customers.image_url
 		ORDER BY customers.name ASC
 	  `);
